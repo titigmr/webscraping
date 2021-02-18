adf02588cca19ed19bfd9b94a80a693a4912e9d7
titigmr <56739959+titigmr@users.noreply.github.com>
2021-02-18 11:56:56 +0100

    Add files via upload

--- GoogleDL.py
+++ GoogleDL.py
@@ -73,41 +73,39 @@ class GoogleImage:
         self.driver.get(url)
         n_downloads = 0
         self.request = request
+        self.n_images = n_images
 
         if self.verbose:
             pbar = tqdm(total=n_images, leave=True)
 
-        while n_downloads < n_images:
-            self._scroll(driver=self.driver, time_sleep=self.time_sleep)
-            all_img = self.driver.find_elements_by_class_name('isv-r')
+        self._scroll(driver=self.driver, time_sleep=self.time_sleep)
+        all_img = self.driver.find_elements_by_class_name('isv-r')
 
-            for n, im in enumerate(all_img):
-                if self._verify_image(im):
-                    im.click()
-                else:
-                    continue
-                img = self.driver.find_element_by_id('islsp')
-                time.sleep(self.time_sleep)
-                soup_img = BeautifulSoup(
-                    img.get_attribute('innerHTML'), 'lxml')
-                balise_link = soup_img.select('img[src*="http"]')
-                if len(balise_link) < 1:
-                    continue
-                link = balise_link[0]["src"]
-                n_str = len(str(n_images))
-                name = f'{request}_{n_downloads:0{n_str}d}'
-                file = self._download_img(link, directory=directory,
-                                          name=name,
-                                          ext_default=self.ext_default,
-                                          make_dir=self.make_dir)
-                n_downloads += 1
-                if self.verbose:
-                    pbar.update(1)
-                self.all_files.append(file)
-                if n > n_images:
-                    break
-            if n_downloads < n_images:
-                self.driver.find_elements_by_class_name('mye4qd').click()
+        for n, im in enumerate(all_img):
+            if self._verify_image(im):
+                im.click()
+            else:
+                continue
+            img = self.driver.find_element_by_id('islsp')
+            time.sleep(self.time_sleep)
+            soup_img = BeautifulSoup(
+                img.get_attribute('innerHTML'), 'lxml')
+            balise_link = soup_img.select('img[src*="http"]')
+            if len(balise_link) < 1:
+                continue
+            link = balise_link[0]["src"]
+            n_str = len(str(n_images))
+            name = f'{request}_{n_downloads:0{n_str}d}'
+            file = self._download_img(link, directory=directory,
+                                        name=name,
+                                        ext_default=self.ext_default,
+                                        make_dir=self.make_dir)
+            n_downloads += 1
+            if self.verbose:
+                pbar.update(1)
+            self.all_files.append(file)
+            if n > n_images:
+                break
         pbar.close()
         if self.close_after_download:
             self.close()
@@ -116,14 +114,18 @@ class GoogleImage:
         last_height = driver.execute_script(
             "return document.body.scrollHeight")
 
-        while True:
+        while len(driver.find_elements_by_class_name('isv-r')) < self.n_images:
             driver.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(time_sleep)
             new_height = driver.execute_script(
                 "return document.body.scrollHeight")
             if new_height == last_height:
-                break
+                actualise = driver.find_element_by_class_name('mye4qd')
+                if actualise.size['height'] > 0:
+                    actualise.click()
+                else:
+                    break
             last_height = new_height
 
     def _download_img(self, link,
